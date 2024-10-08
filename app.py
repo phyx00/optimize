2cabefa34a534e5ed356ec5b0396635516558b7b
phyx00 <phyx00@gmail.com>
2024-10-08 09:23:31 +0300

    Update app.py number of conv

--- app.py
+++ app.py
@@ -227,19 +227,19 @@ def main():
                               step=parameter_ranges['conv_test']['step'])
     
     #conv_boost = st.sidebar.slider('Conv Boost', min_value=10, max_value=2000, value=150, step=10)
-    conv_boost = st.sidebar.slider('Conv Boost',
+    conv_boost = st.sidebar.slider('# of Conv Boost',
                               min_value=parameter_ranges['conv_boost']['min'],
                               max_value=parameter_ranges['conv_boost']['max'],
                               value=10,
                               step=parameter_ranges['conv_boost']['step'])
     #conv_scale = st.sidebar.slider('Conv Scale', min_value=10, max_value=3000, value=200, step=10)
-    conv_scale = st.sidebar.slider('Conv Scale',
+    conv_scale = st.sidebar.slider('# of Conv Scale',
                               min_value=parameter_ranges['conv_scale']['min'],
                               max_value=parameter_ranges['conv_scale']['max'],
                               value=150,
                               step=parameter_ranges['conv_scale']['step'])
     #conv_alpha = st.sidebar.slider('Conv Alpha', min_value=10, max_value=4000, value=300, step=10)
-    conv_alpha = st.sidebar.slider('Conv Alpha',
+    conv_alpha = st.sidebar.slider('# of Conv Alpha',
                               min_value=parameter_ranges['conv_alpha']['min'],
                               max_value=parameter_ranges['conv_alpha']['max'],
                               value=500,
